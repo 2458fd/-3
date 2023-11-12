2d537804168dfad2e2c5fd2858325e49dfd2743d
方润东 <149233981+2458fd@users.noreply.github.com>
2023-11-12 20:40:15 +0800

    Add files via upload

---
+++ 作业二.1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int a[10];
+int main()
+{
+int n,i,j,t;
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+   scanf("%d",&a[i]);
+
+for(i=1;i<n;i++)
+for(j=1;j<=n-i;j++)
+if(a[j]>a[j+1]){  
+  t=a[j];
+  a[j]=a[j+1];
+  a[j+1]=t;
+	}
+for(i=1;i<=n;i++)
+    printf("%d ",a[i]);
+    return 0;
+}
+
+
